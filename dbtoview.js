8a02c46a13d8eb40f28ac062b7589e6072e9f07a
yusup <yusupmail@gmail.com>
2016-11-02 11:25:35 +0700

    part2

--- dbtoview.js
+++ dbtoview.js
@@ -3,11 +3,15 @@ var connection  = require('express-myconnection');
 var http = require('http');
 var mysql = require('mysql');
 var path = require('path');
+var bodyParser = require('body-parser');
+
 //load route
 var route = require('./route'); 
 var app = express();
+
 app.set('views', path.join(__dirname, 'viewstable'));
 app.set('view engine', 'ejs');
+app.set('customvar','content of custom var');
 
 app.use(
     
@@ -20,6 +24,13 @@ app.use(
         database:'testonline'
     },'request')
 );
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
 app.get('/', route.list);
+app.get('/edit/:id',route.edit);
+app.post('/save_edit/:id',route.save);
+app.get('/delete/:id',route.delete);
 
 http.createServer(app).listen(8080);
--- route.js
+++ route.js
@@ -1,15 +1,51 @@
 exports.list = function(req, res){
   req.getConnection(function(err,connection){
-       
      connection.query('SELECT * FROM category',function(err,rows)     {
-            
         if(err)
            console.log("Error Selecting : %s ",err );
-     
-            res.render('viewcategory',{page_title:"Sample list view database",data:rows});
-                           
+            res.render('viewcategory',{page_title:"Sample list view database",data:rows,customvar:req.app.get('customvar')});
          });
-       
     });
-  
 };
+
+exports.edit = function(req, res){
+    var id = req.params.id;
+    req.getConnection(function(err,connection){
+        var query = connection.query('SELECT * FROM category WHERE category_id = ?',[id],function(err,rows)
+        {
+            if(err)
+                console.log("Error Selecting : %s ",err );
+            res.render('editcategory',{page_title:"Edit Category",data:rows});
+         });
+    }); 
+};
+
+exports.save = function(req,res){
+    var input = JSON.parse(JSON.stringify(req.body));
+    var id = req.params.id;
+    req.getConnection(function (err, connection) {
+        var data = {
+            name    : input.category
+        };
+        
+        connection.query("UPDATE category set ? WHERE category_id = ? ",[data,id], function(err, rows)
+        {
+            if (err)
+              console.log("Error Updating : %s ",err );
+          res.redirect('/');
+        });
+    });
+};
+
+exports.delete = function(req,res){
+     var id = req.params.id;
+     req.getConnection(function (err, connection) {
+        connection.query("DELETE FROM category  WHERE category_id = ? ",[id], function(err, rows)
+        {
+             if(err)
+                 console.log("Error deleting : %s ",err );
+             res.redirect('/');
+        });
+     });
+};
+
